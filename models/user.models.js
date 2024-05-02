d1fe161048044e8260dd1150a3a8405111a1d6b9
Pragnya sahu <pragnya110@gmail.com>
2024-05-02 08:09:00 +0530

    register control

--- models/user.models.js
+++ models/user.models.js
@@ -55,10 +55,10 @@ userSchema.pre("save", async function (next) {
   next();
 });
 userSchema.methods.isPsswordCorrect = async function (password) {
-  await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 userSchema.methods.generateAccessToken = function () {
-  jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
@@ -72,7 +72,7 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 userSchema.methods.generateRefreshToken = function () {
-  jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
     },
